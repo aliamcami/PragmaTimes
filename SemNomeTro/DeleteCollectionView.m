74d39f8aeb9a64eae308e40ef6b8f18c2547f94e
Giovani Ferreira Silvério da Silva <giovanifss@MACBEPIDUCB131.local>
2015-04-24 19:12:42 -0300

    nada demais, so um aviso la

--- SemNomeTro/DeleteCollectionView.m
+++ SemNomeTro/DeleteCollectionView.m
@@ -441,8 +441,28 @@ static float const diferenceForAlphaColor = 0.2;
 }
 - (IBAction)remove:(id)sender {
     
-    //salvar dados? n / s
-    [self deleteCellsIndexPaths:selectedArray];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@""
+                                                    message:@"Deseja salvar os dados antes de excluir?"
+                                                   delegate:self
+                                          cancelButtonTitle:@"Cancelar Exclusao"
+                                          otherButtonTitles:@"Salvar",@"Nao Salvar", nil];
+    [alert show];
+}
+
+-(void)alertView:(UIAlertView*)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    switch (buttonIndex) {
+        case 1:
+            for (int i = 0; i < [selectedArray count]; i++) {
+                //Preciso da Camila aqui para resolver isso.
+                [self.shared.arrayChronometers objectAtIndex:[selectedArray objectAtIndex:i]];
+            }
+            
+            [self deleteCellsIndexPaths:selectedArray];
+            break;
+        case 2:
+            [self deleteCellsIndexPaths:selectedArray];
+            break;
+    }
 }
 
 
